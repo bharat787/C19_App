109c461dc159b95c86d01d3910c8f96af1b730c7
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-09 20:37:19 +0530

    staff screen added

--- App.js
+++ App.js
@@ -4,17 +4,43 @@ import HomeUser from './Screens/HomeUser'
 import PersonalDetails from './Screens/PersonalDetails';
 import ProtectMe from './Screens/ProtectMe'
 import LocationHistory from './Screens/LocationHistory'
+import HomeStaff from './Screens/HomeStaff'
+import Login from './Screens/Login'
+import Signup from './Screens/Signup'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import Colors from './constants/colors'
 
 
+function Log({navigation}) {
+	return (
+		<View style={styles.container}>
+			<Login nav={navigation}/>
+		</View>
+	)
+}
+
+function SignUp({navigation}) {
+	return (
+		<View style={styles.container}>
+			<Signup nav={navigation}/>
+		</View>
+	)
+}
 
 function DashBoard({navigation}) {
 	return (
 		<View style={styles.container}>
 			<HomeUser nav={navigation}/>
+		</View>
+	)
+}
+
+function DashBoardStaff({navigation}) {
+	return (
+		<View style={styles.container}>
+			<HomeStaff nav={navigation}/>
 		</View>
 	)
 }
@@ -48,11 +74,32 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-		<Stack.Navigator initialRouteName="UserHome">
+		<Stack.Navigator initialRouteName="LogIn">
         
+		  <Stack.Screen 
+		  name="LogIn" 
+		  component={Log} 
+		  options={{ title: 'Login', headerStyle: { backgroundColor: Colors.BluePrim},
+		  headerTitleAlign: 'center',
+		  headerTintColor: '#fff'}}/>
+
+		  <Stack.Screen 
+		  name="Register" 
+		  component={SignUp} 
+		  options={{ title: 'Register', headerStyle: { backgroundColor: Colors.BluePrim},
+		  headerTitleAlign: 'center',
+		  headerTintColor: '#fff'}}/>
+
 		  <Stack.Screen 
 		  name="UserHome" 
 		  component={DashBoard} 
+		  options={{ title: 'DashBoard', headerStyle: { backgroundColor: Colors.BluePrim},
+		  headerTitleAlign: 'center',
+		  headerTintColor: '#fff'}}/>
+
+		  <Stack.Screen 
+		  name="StaffHome" 
+		  component={DashBoardStaff} 
 		  options={{ title: 'DashBoard', headerStyle: { backgroundColor: Colors.BluePrim},
 		  headerTitleAlign: 'center',
 		  headerTintColor: '#fff'}}/>
