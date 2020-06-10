eefe7f385f585a4958504e48a9e53a9b919d7da0
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-10 22:50:05 +0530

    Update HomeUser.js

--- Screens/HomeUser.js
+++ Screens/HomeUser.js
@@ -1,125 +1,135 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeUser from './Screens/HomeUser'
-import PersonalDetails from './Screens/PersonalDetails';
-import ProtectMe from './Screens/ProtectMe'
-import LocationHistory from './Screens/LocationHistory'
-import HomeStaff from './Screens/HomeStaff'
-import Login from './Screens/Login'
-import Signup from './Screens/Signup'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import Colors from './constants/colors'
+import React from 'react'
+import { StyleSheet, Button, View, Text, TouchableOpacity } from 'react-native'
+import Header from '../components/Header'
+import Colors from '../constants/colors'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
-function Log({navigation}) {
-	return (
-		<View style={styles.container}>
-			<Login nav={navigation}/>
-		</View>
-	)
-}
+const HomeUser = props => {
 
-function SignUp({navigation}) {
-	return (
-		<View style={styles.container}>
-			<Signup nav={navigation}/>
-		</View>
-	)
-}
-
-function DashBoard({navigation}) {
-	return (
-		<View style={styles.container}>
-			<HomeUser nav={navigation}/>
-		</View>
-	)
-}
-
-function PersDetails() {
-	return (
-		<View style={styles.container}>
-			<PersonalDetails/>
-		</View>
-	)
-}
-
-function LocHis() {
-	return (
-		<View style={styles.container}>
-			<LocationHistory/>
-		</View>
-	)
-}
-
-function ProtMe() {
-	return (
-		<View style={styles.container}>
-			<ProtectMe/>
-		</View>
-	)
-}
-
-const Stack = createStackNavigator()
-
-export default function App() {
-  return (
-    <NavigationContainer>
-		<Stack.Navigator initialRouteName="LogIn">
+    return (
         
-		  <Stack.Screen 
-		  name="LogIn" 
-		  component={Log} 
-		  options={{ title: 'Login', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
+        <View style={styles.screen}>
+           
+                
+                <TouchableOpacity 
+                style={styles.button}
+                onPress={() => props.nav.navigate('Protect Me')}>
+                    <Text style={styles.font}>Protect Me</Text>
+                </TouchableOpacity>
 
-		  <Stack.Screen 
-		  name="Register" 
-		  component={SignUp} 
-		  options={{ title: 'Register', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
+                <TouchableOpacity 
+                style={styles.button2}
+                onPress={() => props.nav.navigate('Location History')}>
+                    <Text style={styles.font}>Location History</Text>
+                </TouchableOpacity>
 
-		  <Stack.Screen 
-		  name="UserHome" 
-		  component={DashBoard} 
-		  options={{ title: 'DashBoard', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
+                <TouchableOpacity 
+                style={styles.button3} 
+                onPress={() => props.nav.navigate('Personal Details')}>
+                    <Text style={styles.font}>Personal Details</Text>
+                </TouchableOpacity>
 
-		  <Stack.Screen 
-		  name="Personal Details" 
-		  component={PersDetails}
-		  options={{ title: 'User Details', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
+                <TouchableOpacity 
+                style={styles.logout} 
+                onPress={() => props.nav.navigate('LogIn')}>
+                    <Text style={styles.font}>Logout</Text>
+                </TouchableOpacity>
+                
+            
+        </View>
 
-		  <Stack.Screen 
-		  name="Location History" 
-		  component={LocHis}
-		  options={{ title: 'Places Visited', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
-
-		  <Stack.Screen 
-		  name="Protect Me" 
-		  component={ProtMe}
-		  options={{ title: 'Protect Me', headerStyle: { backgroundColor: Colors.BluePrim},
-		  headerTitleAlign: 'center',
-		  headerTintColor: '#fff'}}/>
-        
-		</Stack.Navigator>
-    </NavigationContainer>
-  );
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create ({
+
+    button: {
+        
+        flex: 1,
+        height: "20%",
+        width: "80%",
+        borderRadius: 20,
+        marginTop: 70,
+        marginBottom: 20,
+        padding: 10,
+        backgroundColor: Colors.Blue1,
+        justifyContent: "center",
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+
+    button2: {
+        
+        flex: 1,
+        height: "20%",
+        width: "80%",
+        borderRadius: 20,
+        marginVertical: 20,
+        padding: 10,
+        backgroundColor: Colors.Blue2,
+        justifyContent: "center",
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+
+    button3: {
+        
+        flex: 1,
+        height: "20%",
+        width: "80%",
+        borderRadius: 20,
+        marginTop: 20,
+        marginBottom: 40,
+        padding: 10,
+        backgroundColor: Colors.Blue3,
+        justifyContent: "center",
+        alignItems: 'center',
+        alignSelf: 'center'
+        
+    },
+
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        padding: 100,
+        flex: 1,
+        width: "130%",
+        marginVertical: 100
+    },
+
+    screen: {
+        flex: 1,
+        width: "100%",
+        justifyContent: 'center'
+    },
+
+    font: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+
+    TopHeader: {
+        backgroundColor: Colors.BluePrim
+    },
+
+    logout: {
+        flex: 0,
+        height: "6%",
+        width: "40%",
+        borderRadius: 15,
+        marginTop: 10,
+        marginBottom: 50,
+        padding: 5,
+        backgroundColor: "#fb5b5a",
+        justifyContent: "center",
+        alignItems: 'center',
+        alignSelf: 'center'
+    }
+
+})
+
+
+export default HomeUser
