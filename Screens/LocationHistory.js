f682d53d006453d20582f519a725603f2d0525ef
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-11 21:30:16 +0530

    Update LocationHistory.js

--- Screens/LocationHistory.js
+++ Screens/LocationHistory.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Button, View, Text, TouchableOpacity, TextComponent } from 'react-native'
 import Header from '../components/Header'
 import Colors from '../constants/colors'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import firebase from '../environment/config'
 
 
 const LocationHistory = props => {
+
+    const [details, setDetails] = useState({})
+    
+    var userDetails = firebase.database().ref(firebase.auth().currentUser.uid)
+    var user = firebase.auth().currentUser.uid
+
+    firebase.database().ref(`users/${firebase.auth().currentUser.uid}/Locations`).once('value', function (snapshot) {
+        setDetails(snapshot.val())
+    });
+    console.log("im here")
+    console.log('see if obj comes after')
+    console.log(details)
 
     return (
         
