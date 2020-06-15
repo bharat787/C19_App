9566df4daaa89ec1189e4e679587e8138070ac25
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-15 21:22:47 +0530

    fetch firebase data

--- Screens/LocationHistory.js
+++ Screens/LocationHistory.js
@@ -1,48 +1,108 @@
-import React, {useState} from 'react'
-import { StyleSheet, Button, View, Text, TouchableOpacity, TextComponent } from 'react-native'
-import Header from '../components/Header'
-import Colors from '../constants/colors'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import firebase from '../environment/config'
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  TouchableOpacity,
+  TextComponent,
+  FlatList,
+} from "react-native";
+import Header from "../components/Header";
+import Colors from "../constants/colors";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import firebase from "../environment/config";
 
+const LocationHistory = (props) => {
+  const [details, setDetails] = useState({});
+  var values = [];
 
-const LocationHistory = props => {
-
-    const [details, setDetails] = useState({})
-    
-    var userDetails = firebase.database().ref(firebase.auth().currentUser.uid)
-    var user = firebase.auth().currentUser.uid
-
-    firebase.database().ref(`users/${firebase.auth().currentUser.uid}/Locations`).once('value', function (snapshot) {
-        setDetails(snapshot.val())
-    });
-    console.log("im here")
-    console.log('see if obj comes after')
-    console.log(details)
-
-    return (
-        
-        <View style={styles.screen}>
-         
-            <Text>Here you will find all places you've logged in.</Text>
-            
-        </View>
-
-    )
-}
-
-const styles = StyleSheet.create ({
-
-    
-
-    screen: {
-        flex: 1,
-        width: "100%",
-        justifyContent: 'center'
+  function isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
     }
+    return true;
+  }
 
-})
+  var userDetails = firebase.database().ref(firebase.auth().currentUser.uid);
+  var user = firebase.auth().currentUser.uid;
 
+  const getLocationData = async () => {
+    let locData = await firebase
+      .database()
+      .ref(`users/${firebase.auth().currentUser.uid}/Locations/`)
+      .once("value");
 
-export default LocationHistory
+    firebase
+      .database()
+      .ref(`users/${firebase.auth().currentUser.uid}/Locations/`)
+      .once("value")
+      .then((snapshot) => {
+        setDetails(snapshot.val());
+      });
+  };
+
+  console.log(getLocationData());
+  if (isEmpty(details)) {
+    console.log("value of  details is NULL ");
+  } else {
+    console.log("value of details is not null ", details);
+    values = Object.values(details);
+    console.log("VALUES ", values);
+  }
+
+  const renderData = (vals) => {
+    return (
+      <View style={styles.feedItem}>
+        <View>
+          <Text style={styles.fontLoc}>{vals.LocationDetails.Location}</Text>
+          <Text style={styles.fontDate}>
+            {vals.LocationDetails.Date +
+              "/" +
+              vals.LocationDetails.Month +
+              "/" +
+              vals.LocationDetails.Year}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
+      <FlatList data={values} renderItem={({ item }) => renderData(item)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+  },
+
+  feedItem: {
+    backgroundColor: Colors.Blue2,
+    borderRadius: 15,
+    padding: 8,
+    flexDirection: "row",
+    marginVertical: 10,
+    width: "80%",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+
+  fontLoc: {
+    fontSize: 24,
+    color: "white",
+  },
+
+  fontDate: {
+    fontSize: 10,
+    color: "white",
+  },
+});
+
+export default LocationHistory;
