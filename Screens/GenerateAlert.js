052e95e8173f8091a495db3c7c41378fe006b455
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-16 21:58:09 +0530

    ui layout

--- Screens/GenerateAlert.js
+++ Screens/GenerateAlert.js
@@ -1,12 +1,8 @@
 import React, {useState} from 'react'
-import { StyleSheet, Button, View, Text, TouchableOpacity, TextComponent } from 'react-native'
-import Header from '../components/Header'
+import { StyleSheet, Button, View, Text, TouchableOpacity, TextComponent, FlatList } from 'react-native'
 import Colors from '../constants/colors'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { greaterOrEq } from 'react-native-reanimated'
 import DateTimePicker from '@react-native-community/datetimepicker';
-import firebase from '../environment/config'
+//import firebase from '../environment/config'
 
 const GenerateAlert = props => {
 
@@ -43,67 +39,100 @@ const GenerateAlert = props => {
       return true;
     }
 
-    // var userDetails = firebase.database().ref(firebase.auth().currentUser.uid);
-    // var user = firebase.auth().currentUser.uid;
-
-    // const getLocationData = async () => {
-    //   let locData = await firebase
-    //     .database()
-    //     .ref(`users/${firebase.auth().currentUser.uid}/Visitors/`)
-    //     .once("value");
-
-    //   firebase
-    //     .database()
-    //     .ref(`users/${firebase.auth().currentUser.uid}/Visitors/`)
-    //     .once("value")
-    //     .then((snapshot) => {
-    //       setDetails(snapshot.val());
-    //     });
-    // };
-
-    // console.log(getLocationData());
-    // if (isEmpty(details)) {
-    //   console.log("value of  details is NULL ");
-    // } else {
-    //   console.log("value of details is not null ", details);
-    //   values = Object.values(details);
-    //   console.log("VALUES ", values);
-    // }
-
-
+    
     return (
-      <View style={styles.screen}>
-        <View>
-        	<View>
-            	<Button onPress={showDatepicker} title="Pick Date" />
-        	</View>
-        	<View>
-            	<Button onPress={showTimepicker} title="Pick Time" />
-        	</View>
-          		{show && (
-            		<DateTimePicker
-            			testID="dateTimePicker"
-              			value={date}
-              			mode={mode}
-              			is24Hour={true}
-             	 		display="default"
-              			onChange={onChange}
-            								/>
-          		)}
-        	</View>
-      </View>
+		<View style={styles.screen}>
+				<View style={styles.flatView}>
+					<FlatList />
+				</View>
+				<View >
+					<View>
+						<TouchableOpacity
+						style = {styles.buttons} 
+						onPress={showDatepicker}>
+							<Text>Pick a Date</Text>
+						</TouchableOpacity>
+					
+						<TouchableOpacity 
+						style = {styles.buttons} 
+						onPress={showTimepicker}>
+							<Text>Pick a Time</Text>
+						</TouchableOpacity>
+						<TouchableOpacity style={styles.AlertButton}>
+							<Text>Send Alert Notification</Text>
+						</TouchableOpacity>
+					</View>
+						{show && (
+							<DateTimePicker
+								testID="dateTimePicker"
+								value={date}
+								mode={mode}
+								is24Hour={true}
+								display="default"
+								onChange={onChange}
+													/>
+						)}
+				</View>
+				
+								
+		</View>
     );
 }
 
 const styles = StyleSheet.create ({
 
-    
+	flatView: {
+		flex: 0,
+		backgroundColor: Colors.Blue1,
+		marginTop: 140,
+		marginBottom: 30,
+		marginHorizontal: 20,
+		height: 450
+	},
 
     screen: {
         flex: 1,
         width: "100%",
-        justifyContent: 'center'
-    }
+		justifyContent: 'center',
+		backgroundColor: Colors.TealText
+	},
+	
+	buttons: {
+		flex: 0,
+		width: "80%",
+		height: "15%",
+		justifyContent: 'center',
+		alignSelf: 'center',
+		borderRadius: 20,
+		padding: 20,
+		backgroundColor: Colors.Blue1,
+		fontSize: 20,
+		margin: 20
+
+				},
+
+	buttonView: {
+		flex: 0,
+		width: '70%',
+		flexDirection: 'row',
+		
+		alignSelf: 'center'
+		
+	},
+
+	AlertButton: {
+		
+		flex: 0,
+		width: "80%",
+		height: "25%",
+		justifyContent: 'center',
+		alignSelf: 'center',
+		borderRadius: 20,
+		padding: 20,
+		backgroundColor: Colors.RedPrim,
+		fontSize: 20,
+		margin: 20
+	}
 
 })
 
