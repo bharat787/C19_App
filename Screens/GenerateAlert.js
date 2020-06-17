aea4a936a23fa30febcbd5e64b32999ad8513274
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-17 20:40:16 +0530

    Update GenerateAlert.js

--- Screens/GenerateAlert.js
+++ Screens/GenerateAlert.js
@@ -1,80 +1,74 @@
 import React, {useState} from 'react'
-import { StyleSheet, Button, View, Text, TouchableOpacity, TextComponent, FlatList } from 'react-native'
+import { StyleSheet, Button, View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native'
 import Colors from '../constants/colors'
 import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from "react-native-modal-datetime-picker"
 //import firebase from '../environment/config'
 
 const GenerateAlert = props => {
 
-    const [details, setDetails] = useState({});
-    var values = [];
-    const [date, setDate] = useState(new Date(1598051730000));
-    const [mode, setMode] = useState("date");
-    const [show, setShow] = useState(false);
 
-    const onChange = (event, selectedDate) => {
-      const currentDate = selectedDate || date;
-      setShow(Platform.OS === "ios");
-	  setDate(currentDate);
-	  console.log(currentDate)
-    };
+	var date = new Date().getDate(); //Current Date
+	var month = new Date().getMonth() + 1; //Current Month
+	var year = new Date().getFullYear(); //Current 
+	var hours = new Date().getHours()
 
-    const showMode = (currentMode) => {
-      setShow(true);
-      setMode(currentMode);
-    };
+	var [selectDate, setSelectDate]  = useState('date')
+	var [selectMonth, setSelectMonth]  = useState('month')
+	var [selectYear, setSelectYear]  = useState('year')
+	var [selectHour, setSelectHour]  = useState('hours')
 
-    const showDatepicker = () => {
-      showMode("date");
-    };
+	const SearchLogs = () => {
+		console.log("search log",selectDate + '/' + selectMonth + '/' + selectYear+ '---' + selectHour)
+	}
 
-    const showTimepicker = () => {
-      showMode("time");
-    };
-
-    function isEmpty(obj) {
-      for (var key in obj) {
-        if (obj.hasOwnProperty(key)) return false;
-      }
-      return true;
-    }
-
-    
     return (
 		<View style={styles.screen}>
-				<View style={styles.flatView}>
-					<FlatList />
-				</View>
-				<View >
-					<View>
-						<TouchableOpacity
-						style = {styles.buttons} 
-						onPress={showDatepicker}>
-							<Text>Pick a Date</Text>
-						</TouchableOpacity>
-					
-						<TouchableOpacity 
-						style = {styles.buttons} 
-						onPress={showTimepicker}>
-							<Text>Pick a Time</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.AlertButton}>
-							<Text>Send Alert Notification</Text>
-						</TouchableOpacity>
-					</View>
-						{show && (
-							<DateTimePicker
-								testID="dateTimePicker"
-								value={date}
-								mode={mode}
-								is24Hour={true}
-								display="default"
-								onChange={onChange}
-													/>
-						)}
-				</View>
-				
-								
+			<View style={styles.flatView}>
+				<FlatList />
+			</View>
+			<View style={styles.DateView}>
+				<Text style={styles.textin}>DD:</Text>
+				<TextInput
+				style={styles.inputText}
+				placeholder='DD'
+				keyboardType='numeric'
+				onChangeText={text => setSelectDate(text)}/>
+
+				<Text style={styles.textin}>MM:</Text>
+				<TextInput
+				style={styles.inputText}
+				placeholder='MM'
+				keyboardType='numeric'
+				onChangeText={text => setSelectMonth(text)}/>
+
+				<Text style={styles.textin}>YYYY:</Text>
+				<TextInput
+				style={styles.inputText}
+				placeholder='YYYY'
+				keyboardType='numeric'
+				onChangeText={text => setSelectYear(text)}/>
+
+				<Text style={styles.textin}>HRS:</Text>
+				<TextInput
+				style={styles.inputText}
+				placeholder='HRS'
+				keyboardType='numeric'
+				onChangeText={text => setSelectHour(text)}/>
+			</View>
+
+			<View style={styles.buttonView}>
+				<TouchableOpacity 
+					style={styles.searchButton}
+					onPress={() => SearchLogs()}>
+					<Text 
+					style={styles.btnfont}>Search</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={styles.AlertButton}>
+					<Text style={styles.btnfont}>Alert</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
     );
 }
@@ -82,7 +76,7 @@ const GenerateAlert = props => {
 const styles = StyleSheet.create ({
 
 	flatView: {
-		flex: 0,
+		//flex: 0,
 		backgroundColor: Colors.Blue1,
 		marginTop: 140,
 		marginBottom: 30,
@@ -94,7 +88,7 @@ const styles = StyleSheet.create ({
         flex: 1,
         width: "100%",
 		justifyContent: 'center',
-		backgroundColor: Colors.TealText
+		backgroundColor: '#fff'
 	},
 	
 	buttons: {
@@ -113,7 +107,7 @@ const styles = StyleSheet.create ({
 
 	buttonView: {
 		flex: 0,
-		width: '70%',
+		width: '100%',
 		flexDirection: 'row',
 		
 		alignSelf: 'center'
@@ -123,15 +117,58 @@ const styles = StyleSheet.create ({
 	AlertButton: {
 		
 		flex: 0,
-		width: "80%",
+		width: "40%",
 		height: "25%",
 		justifyContent: 'center',
 		alignSelf: 'center',
 		borderRadius: 20,
 		padding: 20,
 		backgroundColor: Colors.RedPrim,
+		margin: 20,
+		alignItems: 'center'
+	},
+
+	DateView: {
+		flexDirection: 'row',
+		justifyContent: 'center'
+
+	},
+
+	textin: {
+		padding: 5,
+		margin: 5,
+		alignItems: 'center'
+	},
+
+	inputText: {
+		padding: 5,
+		margin: 5,
+		borderColor: 'white',
+		borderWidth: 1,
+		justifyContent: 'center',
+		alignSelf: 'center',
+		borderRadius: 10,
+		alignItems: 'center'
+	},
+
+	searchButton: {
+		
+		flex: 0,
+		width: "40%",
+		height: "25%",
+		justifyContent: 'center',
+		alignSelf: 'center',
+		borderRadius: 20,
+		padding: 20,
+		backgroundColor: Colors.Tealbg,
+		margin: 20,
+		alignItems: 'center'
+	},
+
+	btnfont: {
+		color: 'white',
 		fontSize: 20,
-		margin: 20
+		fontWeight: 'bold'
 	}
 
 })
