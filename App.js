678ffb7f0d201483683097a525fd40ac6cbf9f2a
bharat787 <40318290+bharat787@users.noreply.github.com>
2020-06-22 20:59:51 +0530

    OneSignal notification system enabled

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import 'react-native-gesture-handler';
 import Colors from './constants/colors'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import OneSignal from 'react-native-onesignal'
 
 const intialState = [
 	{Staff: undefined}
@@ -124,7 +125,40 @@ const Stack = createStackNavigator()
 
 export default function App() {
 
+	//Remove this method to stop OneSignal Debugging 
+	OneSignal.setLogLevel(6, 0);
+  
+	// Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+	OneSignal.init("aa868a29-ea69-43f8-ab84-ea7e0ed97a47", {kOSSettingsKeyAutoPrompt : false, kOSSettingsKeyInAppLaunchURL: false, kOSSettingsKeyInFocusDisplayOption:2});
+	OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
 	
+	// The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+	//OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+  
+	OneSignal.addEventListener('received', onReceived);
+	OneSignal.addEventListener('opened', onOpened);
+	OneSignal.addEventListener('ids', onIds);
+  
+  
+	OneSignal.removeEventListener('received', onReceived);
+	OneSignal.removeEventListener('opened', onOpened);
+	OneSignal.removeEventListener('ids', onIds);
+  
+  
+	const onReceived = (notification) => {
+	  console.log("Notification received: ", notification);
+	}
+  
+	const onOpened = (openResult) => {
+	  console.log('Message: ', openResult.notification.payload.body);
+	  console.log('Data: ', openResult.notification.payload.additionalData);
+	  console.log('isActive: ', openResult.notification.isAppInFocus);
+	  console.log('openResult: ', openResult);
+	}
+  
+	const onIds = (device) => {
+	  console.log('Device info: ', device);
+	}
 	
 
 
